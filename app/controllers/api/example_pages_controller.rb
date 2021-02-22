e0f8f5efe32a88d2f4da005a9e3ff95d2700a0e3
Chelsea Roston <cmroston@gmail.com>
2021-02-22 15:22:50 -0500

    tests out rendering json

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,9 @@
 class Api::ExamplePagesController < ApplicationController
+  def hello_action
+    render json: [{ english: "hello", spanish: "hola", mandrain: "nihao" }]
+  end
+
+  def information
+    render json: [{ f_name: "Chelsea", l_name: "Roston-Rappazzo", hobbies: ["reading", "K-pop", "costuming"] }]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/hello_path" => "example_pages#hello_action"
+  end
+
+  namespace :api do
+    get "/information" => "example_pages#information"
+  end
 end
